229f4842396d66eabd62347dcd7a5c89d6905baf
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2022-08-30 15:06:09 +0300

    Add project files.

---
+++ Kordamine_OOP_1/Koer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_OOP_1
+{
+    class Koer : Koduloom
+    {
+        public string toug;//tõug-порода
+        //public Koer(string toug, string nimi, string varv, char sugu, double kaal, int vanus, bool elav) :base(nimi,varv,sugu,kaal,vanus,elav)  
+        //{
+        //    this.toug = toug;
+        //}
+        public Koer(string toug, string nimi, string varv, sugu loomaSugu, double kaal, int vanus, bool elav) : base(nimi, varv, loomaSugu, kaal, vanus, elav)
+        {
+            this.toug = toug;
+        }
+        //public override void print_Info()
+        //{
+        //    string sugu_text = "";
+        //    if (sugu == 'I')
+        //    {
+        //        sugu_text = "Isane";
+        //    }
+        //    else if (sugu == 'E')
+        //    {
+        //        sugu_text = "Emane";
+        //    }
+        //    Console.WriteLine("{0} {1} {2} ta on {3} ja tema kaal on {4} ja ta on {5} aastat vana", toug, varv, nimi, sugu_text, kaal, vanus);
+        //}
+        public override void print_Info()
+        {
+            Console.WriteLine("{0} {1} {2} ta on {3} ja tema kaal on {4} ja ta on {5} aastat vana", toug, varv, nimi, loomaSugu, kaal, vanus);
+        }
+    }
+}
---
+++ Kordamine_OOP_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Kordamine_OOP_1;
+
+Console.WriteLine("Hello, World!");
+//Koduloom kass = new Koduloom("Murka", "punane", 'E', 5.5, 7, true);
+//kass.print_Info();
+//Koduloom kassKloon=new Koduloom(kass);
+//kassKloon.muuda_Nimi("MurkaKloon");
+//kassKloon.print_Info();
+Koer koer = new Koer("lambakoer", "Šarik", "punane", Koduloom.sugu.isane, 35.5, 7, true);
+koer.print_Info();
